a10fe3168d858f198dce6fb12989952d71f202fa
Luis Yoshida <luis.yoshida@gmail.com>
2022-03-01 11:32:54 +0000

    Finish crispy forms

--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
