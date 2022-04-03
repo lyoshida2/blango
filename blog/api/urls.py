ca38db43c0accec6185f8b7b8d62600653d958cd
Luis Yoshida <luis.yoshida@gmail.com>
2022-04-03 16:17:34 +0000

    Finish simplejwt

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -6,6 +6,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
@@ -52,4 +53,6 @@ urlpatterns += [
         PostViewSet.as_view({"get": "list"}),
         name="posts-by-time",
     ),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),
 ]
